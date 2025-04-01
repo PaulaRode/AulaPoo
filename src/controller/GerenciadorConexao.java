c26b6381ba7f41e9e6e27d68db5262c5a6e23160
PaulaRode <paulamrode@gmail.com>
2025-03-31 22:38:14 -0300

    Primeira aula

--- src/controller/GerenciadorConexao.java
+++ src/controller/GerenciadorConexao.java
@@ -13,6 +13,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
+
+//estpi temtamdp emvoar pro git
 public class GerenciadorConexao {
 
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/dbprojeto";
